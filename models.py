dbd09ceb09338dbf6aa6d44f6f80f90366ac853e
zilongg-wu <79116846+zilongg-wu@users.noreply.github.com>
2022-09-28 22:53:07 +1300

    Add files via upload
    added models.py

---
+++ models.py
@@ -0,0 +1,52 @@
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from views import db
+
+
+
+
+# Models
+# User Model
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    classrooms = db.relationship(
+        'Classroom', backref='user', passive_deletes=True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
+
+
+# Classroom Model
+class Classroom(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    end_user = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    comments = db.relationship(
+        'Comment', backref='classroom', passive_deletes=True)
+    likes = db.relationship('Like', backref='classroom', passive_deletes=True)
+
+
+# Comment Model
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(35), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    end_user = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    classroom_id = db.Column(db.Integer, db.ForeignKey(
+        'classroom.id', ondelete="CASCADE"), nullable=False)
+
+
+# Like Model
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    end_user = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    classroom_id = db.Column(db.Integer, db.ForeignKey(
+        'classroom.id', ondelete="CASCADE"), nullable=False)
